bca4a133e98eb3dc813027ae2f23c536736cecca
Hussein Nasrallah <109015272+Husein81@users.noreply.github.com>
2025-11-16 20:18:49 +0200

    wip

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,18 +1,11 @@
-"use client";
-
-import Link from "next/link";
-import NavUser from "./NavUser";
-import Image from "next/image";
-import NavItems from "./NavItems";
-import { useTheme } from "next-themes";
-import { useEffect, useEffectEvent, useState } from "react";
 import Logo from "./Logo";
+import NavItems from "./NavItems";
+import NavUser from "./NavUser";
 
 const Navbar = () => {
-  
   return (
     <nav className="flex items-center z-50 justify-between mx-auto py-4 px-6 bg-background shadow-md opacity-90 sticky top-0">
-     <Logo  />
+      <Logo />
       <NavItems />
 
       <NavUser />
